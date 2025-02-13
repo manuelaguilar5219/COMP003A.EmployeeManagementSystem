3ec0d9a0d72d3e1623dd42be2b44a2fcb7255df0
manuelaguilar5219 <manuelaguilar5219@LEM231-31>
2025-02-12 20:37:41 -0800

    Add project files.

---
+++ COMP003A.EmployeeManagementSystem/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.EmployeeManagementSystem
+{
+    public class Employee
+    {
+        private string _employeeId;
+        private string _firstName;
+        private string _middleName = "";
+        private string _lastName;
+        private double _salary;
+
+        public string EmployeeId
+        {
+            get { return _employeeId; }
+        }
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Your first name can't be blank!");
+    
+            }
+                _firstName = value;
+            }
+        }
+        public string MiddleName
+        {
+            get { return _middleName; }
+            set
+            {
+                _middleName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Your last name can't be blank!");
+                }
+                _lastName = value;
+            }
+        }
+        public double Salary
+        {
+            get { return _salary; }
+            set
+            {
+                if (Salary < 0)
+                {
+                    throw new ArgumentException("Salary can't be less than 0!");
+    
+            }
+                _salary = value;
+            }
+        }
+    }
+}
