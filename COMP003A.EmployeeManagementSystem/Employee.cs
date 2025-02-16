cdb0df64c612c01b2d29bf438b28b4d957c2c7d0
manuelaguilar5219 <manuelaguilar5219@my.whccd.edu>
2025-02-15 18:25:13 -0800

    Added XML comments to Employee.cs

--- COMP003A.EmployeeManagementSystem/Employee.cs
+++ COMP003A.EmployeeManagementSystem/Employee.cs
@@ -16,7 +16,14 @@ namespace COMP003A.EmployeeManagementSystem
         private string _middleName = "";
         private string _lastName;
         private double _salary;
-
+        /// <summary>
+        /// Constructor that initializes all fields, applying validation through the properties.
+        /// </summary>
+        /// <param name="employeeId">The empoloyee's ID</param>
+        /// <param name="firstName">The employee's first name</param>
+        /// <param name="middleName">The employee's middle name</param>
+        /// <param name="lastName">The employees's Last name</param>
+        /// <param name="salary">The employee's salary</param>
         public Employee(string employeeId, string firstName, string middleName, string lastName, double salary)
         {
             _employeeId = employeeId;
@@ -25,10 +32,16 @@ namespace COMP003A.EmployeeManagementSystem
             LastName = lastName;
             Salary = salary;
         }
+        /// <summary>
+        /// Gets the employee id only.
+        /// </summary>
         public string EmployeeId
         {
             get { return _employeeId; }
         }
+        /// <summary>
+        /// Gets and sets the employee's fast name. Throws ArgumentException that displays a message that the fast name cannot be blank
+        /// </summary>
         public string FirstName
         {
             get { return _firstName; }
@@ -42,6 +55,9 @@ namespace COMP003A.EmployeeManagementSystem
                 _firstName = value;
             }
         }
+        /// <summary>
+        /// Gets and sets the employee's middle name. 
+        /// </summary>
         public string MiddleName
         {
             get { return _middleName; }
@@ -50,6 +66,9 @@ namespace COMP003A.EmployeeManagementSystem
                 _middleName = value;
             }
         }
+        /// <summary>
+        /// Gets and sets the employee's last name. Throws ArgumentException that displays a message that the last name cannot be blank
+        /// </summary>
         public string LastName
         {
             get { return _lastName; }
@@ -62,6 +81,9 @@ namespace COMP003A.EmployeeManagementSystem
                 _lastName = value;
             }
         }
+        /// <summary>
+        /// Gets and sets the Salary. Throws ArgumentException that displays message that the value cannot be less than 0.
+        /// </summary>
         public double Salary
         {
             get { return _salary; }
@@ -74,6 +96,9 @@ namespace COMP003A.EmployeeManagementSystem
                 _salary = value;
             }
         }
+        /// <summary>
+        /// Method that prints the employee's full name.
+        /// </summary>
         public void PrintFullName()
         {
             if (string.IsNullOrEmpty(MiddleName))
@@ -85,6 +110,9 @@ namespace COMP003A.EmployeeManagementSystem
                 Console.WriteLine($"{FirstName} {MiddleName} {LastName}");
             }
         }
+        /// <summary>
+        /// Method that displays the employee's info.
+        /// </summary>
         public void DisplayEmployeeInfo()
         {
             Console.WriteLine($"Employee ID: {EmployeeId}");
